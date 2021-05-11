f6eada6fb057483e184fbcaeefe172f874e6d541
yerimkoko <gochi97@naver.com>
2021-05-11 19:56:33 +0900

    Initial commit

---
+++ src/test/java/com/potatoes/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoes.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
